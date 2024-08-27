1f47491a7cdd0bcdaae843b23c5c11a70addad01
Rei Ramadani <ramadanirei@gmail.com>
2024-08-27 14:57:23 +0200

    Creating components and fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import NoteInput from "./Components/NoteInput";
 function App() {
-  return <div>Welcome</div>;
+  return (
+    <div className={"container"}>
+      <Header />
+      <NoteInput />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./Search";
+
+function Header() {
+  return (
+    <div>
+      <div className="Header-text"> Notes Application</div>
+      <Search />
+    </div>
+  );
+}
+export default Header;
